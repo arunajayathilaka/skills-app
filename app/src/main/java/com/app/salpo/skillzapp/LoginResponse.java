82e4492c642c3b8d4e497691426597459aadf57f
imal <imalkseven@gmail.com>
2017-08-23 19:02:58 +0530

    onLoginClick

---
+++ app/src/main/java/com/app/salpo/skillzapp/LoginResponse.java
@@ -0,0 +1,46 @@
+package com.app.salpo.skillzapp;
+
+/**
+ * Created by imalkariyawasam on 8/23/17.
+ */
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class LoginResponse {
+
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("data")
+    @Expose
+    private Data data;
+    @SerializedName("msg")
+    @Expose
+    private String msg;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+}
